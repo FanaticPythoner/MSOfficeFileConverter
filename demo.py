a59e8124a34ef353dbb01c4f6346139ed1eeab14
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-10 18:07:24 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,7 @@
 from MSOfficeFileConverter import WordDocument
 
 #Creating the WordDocument object
-
 document = WordDocument('Example\Path\To\file.docx')
 
 #Exporting to PDF
-
 document.toPdf('Example\Export\Path','OutputFileName')
