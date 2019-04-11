1ee534ac3fc2e8cd2e70c8109eae9667e05b7fda
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-10 22:52:09 -0400

    Update codeSample.py

--- codeSample.py
+++ codeSample.py
@@ -1,12 +1,28 @@
 from MSOfficeFileConverter import WordDocument, ExcelDocument
 
-#Creating the WordDocument object
-document = WordDocument('Example\Path\To\file.docx')
-#Exporting to PDF
-document.toPdf('Example\Export\Path','OutputFileName')
+# Creating the WordDocument object.
+document = WordDocument('SampleWord.docx')
+
+# Exporting to PDF.
+document.toPdf('ExportFolder','OutputSampleWord.pdf')
+
+# Exporting to Html. Notice the new folder created in ExportFolder.
+document.toHtml('ExportFolder', 'OutputSampleWord.html')
 
 
-#Creating the ExcelDocument object
-document = ExcelDocument('Example\Path\To\file.xlsx')
-#Exporting to PDF
-document.toPdf('Example\Export\Path','OutputFileName')
+# ======================================================================================================
+
+
+# Creating the ExcelDocument object.
+document = ExcelDocument('SampleExcel.xlsx')
+
+# Exporting to PDF. Notice that even if we dont specify the file extension, the export still work.
+document.toPdf('ExportFolder','SampleExcel.pdf')
+
+# Exporting to XLS. Notice that if we dont give any arguments, the tool export the file in the same
+# directory as the original Excel file and with the same name as the original Excel file.
+document.toXls()
+
+# Exporting to CSV. Notice the new folder created in ExportFolder with all the spreadsheets exported, 
+# instead of only the selected one being exported as default in Excel.
+document.toCsv('ExportFolder','SampleExcel.csv')
