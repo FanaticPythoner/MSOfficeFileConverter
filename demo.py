fca34b78819da3a9c85dec88331b58e211acd8f8
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-10 22:55:48 -0400

    Update and rename codeSample.py to demo.py

--- demo.py
+++ demo.py
@@ -4,10 +4,10 @@ from MSOfficeFileConverter import WordDocument, ExcelDocument
 document = WordDocument('SampleWord.docx')
 
 # Exporting to PDF.
-document.toPdf('ExportFolder','OutputSampleWord.pdf')
+document.toPdf('demo_ExportFolder','OutputSampleWord.pdf')
 
 # Exporting to Html. Notice the new folder created in ExportFolder.
-document.toHtml('ExportFolder', 'OutputSampleWord.html')
+document.toHtml('demo_ExportFolder', 'OutputSampleWord.html')
 
 
 # ======================================================================================================
@@ -17,7 +17,7 @@ document.toHtml('ExportFolder', 'OutputSampleWord.html')
 document = ExcelDocument('SampleExcel.xlsx')
 
 # Exporting to PDF. Notice that even if we dont specify the file extension, the export still work.
-document.toPdf('ExportFolder','SampleExcel.pdf')
+document.toPdf('demo_ExportFolder','SampleExcel.pdf')
 
 # Exporting to XLS. Notice that if we dont give any arguments, the tool export the file in the same
 # directory as the original Excel file and with the same name as the original Excel file.
@@ -25,4 +25,4 @@ document.toXls()
 
 # Exporting to CSV. Notice the new folder created in ExportFolder with all the spreadsheets exported, 
 # instead of only the selected one being exported as default in Excel.
-document.toCsv('ExportFolder','SampleExcel.csv')
+document.toCsv('demo_ExportFolder','SampleExcel.csv')
