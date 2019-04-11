0f51d425df01942b09f8b761ef6aa3b3442a6e55
Nathan Trudeau <45826736+FanaticPythoner@users.noreply.github.com>
2019-04-10 21:35:28 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
-from MSOfficeFileConverter import WordDocument
+from MSOfficeFileConverter import WordDocument, ExcelDocument
 
 #Creating the WordDocument object
 document = WordDocument('Example\Path\To\file.docx')
+#Exporting to PDF
+document.toPdf('Example\Export\Path','OutputFileName')
 
+
+#Creating the ExcelDocument object
+document = ExcelDocument('Example\Path\To\file.xlsx')
 #Exporting to PDF
 document.toPdf('Example\Export\Path','OutputFileName')
